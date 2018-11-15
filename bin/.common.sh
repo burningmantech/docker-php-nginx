3471e99aa76daf355e1ab4a85256c08a8ec75382
Wilfredo Sánchez <wsanchez@wsanchez.net>
2018-11-15 14:38:44 -0800

    Set container_name

--- bin/.common.sh
+++ bin/.common.sh
@@ -5,3 +5,5 @@ wd="$(cd "$(dirname "$0")/.." && pwd)";
 image_repo="php-nginx";
  image_tag="$(cat "${wd}/Dockerfile" | grep -e '^FROM ' | sed -e 's|^FROM php:||' -e 's|-fpm||')";
 image_name="${image_repo}:${image_tag}";
+
+container_name="php-nginx";
