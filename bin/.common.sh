5527ea35544745c78731b000bb766c994fc1dbce
Wilfredo Sánchez <wsanchez@wsanchez.net>
2018-11-15 14:36:39 -0800

    Add .common.sh

---
+++ bin/.common.sh
@@ -0,0 +1,7 @@
+set -eu
+
+wd="$(cd "$(dirname "$0")/.." && pwd)";
+
+image_repo="php-nginx";
+ image_tag="$(cat "${wd}/Dockerfile" | grep -e '^FROM ' | sed -e 's|^FROM php:||' -e 's|-fpm||')";
+image_name="${image_repo}:${image_tag}";
